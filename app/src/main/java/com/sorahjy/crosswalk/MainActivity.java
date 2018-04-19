f19b6b2a4ed6f421aa446a11e80a5e6b3ed7d5ab
sorahjy <sorahjy@gmail.com>
2018-04-19 21:28:46 +0800

    init

---
+++ app/src/main/java/com/sorahjy/crosswalk/MainActivity.java
@@ -0,0 +1,49 @@
+package com.sorahjy.crosswalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.xwalk.core.XWalkPreferences;
+import org.xwalk.core.XWalkView;
+
+public class MainActivity extends AppCompatActivity {
+    private XWalkView xWalkWebView;
+    private String url="https://www.sorahjy.com";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        xWalkWebView = findViewById(R.id.xwalkWebView);
+        // turn on debugging
+        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+        xWalkWebView.load(url, null);
+        // load local html file
+        // xWalkWebView.load("localfile.html", null);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (xWalkWebView != null) {
+            xWalkWebView.pauseTimers();
+            xWalkWebView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (xWalkWebView != null) {
+            xWalkWebView.resumeTimers();
+            xWalkWebView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (xWalkWebView != null) {
+            xWalkWebView.onDestroy();
+        }
+    }
+}
