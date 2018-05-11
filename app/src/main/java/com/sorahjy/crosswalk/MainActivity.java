cd5581bade80bad6e647035135bd8535e6d2d320
sorahjy <sorahjy@gmail.com>
2018-05-11 14:02:29 +0800

    init

--- app/src/main/java/com/sorahjy/crosswalk/MainActivity.java
+++ app/src/main/java/com/sorahjy/crosswalk/MainActivity.java
@@ -1,48 +1,109 @@
 package com.sorahjy.crosswalk;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import org.xwalk.core.XWalkPreferences;
-import org.xwalk.core.XWalkView;
 
 public class MainActivity extends AppCompatActivity {
-    private XWalkView xWalkWebView;
+    private WebView webView;
     //这个url就是你想要打开的页面。
-    private String url="https://www.sorahjy.com";
+   private String url = "https://class.sorahjy.com/";
+    //private String url="https://jvb.nobler.me/ntm";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        xWalkWebView = findViewById(R.id.xwalkWebView);
-        // turn on debugging
-        XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
-        xWalkWebView.load(url, null);
+        WebView myWebView = findViewById(R.id.webview);
+        webView = myWebView;
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setUserAgentString("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36");
+        setUpWebViewDefaults(webView);
+        final Activity activity = this;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO},
+                1);
+        myWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+        myWebView.loadUrl(url);
+
+    }
+
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+
+        // Enable Javascript
+        settings.setJavaScriptEnabled(true);
+
+        // Use WideViewport and Zoom out if there is no viewport defined
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+        // Enable pinch to zoom without the zoom buttons
+        settings.setBuiltInZoomControls(true);
+
+        // Allow use of Local Storage
+        settings.setDomStorageEnabled(true);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.HONEYCOMB) {
+            // Hide the zoom controls for HONEYCOMB+
+            settings.setDisplayZoomControls(false);
+        }
+
+        // Enable remote debugging via chrome://inspect
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // AppRTC requires third party cookies to work
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (xWalkWebView != null) {
-            xWalkWebView.pauseTimers();
-            xWalkWebView.onHide();
+        if (webView != null) {
+            webView.pauseTimers();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (xWalkWebView != null) {
-            xWalkWebView.resumeTimers();
-            xWalkWebView.onShow();
+        if (webView != null) {
+            webView.resumeTimers();
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (xWalkWebView != null) {
-            xWalkWebView.onDestroy();
+        if (webView != null) {
         }
     }
 }
